480d18e679273fa5d5d76ef1053712a58cc16631
Justin Kim <jk07661@gmail.com>
2022-01-12 17:50:14 -0500

    Added basic homepage with navbar

--- client/App.js
+++ client/App.js
@@ -1,15 +1,19 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
+import Home from "./components/HomePage";
+import SinglePage from "./components/SinglePage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
       <Routes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
