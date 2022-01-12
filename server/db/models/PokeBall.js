29e0a2de60431211e5e17c7314cfab6ab4807197
Sam Kanan <samkanan@gmail.com>
2022-01-12 16:47:07 -0600

    created user and pokeball models; created association table; created seed file; create initial routes for pokeball model

---
+++ server/db/models/PokeBall.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const PokeBall = db.define('pokeBall', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: '../../../../public/images/image-coming-soon.jpeg'
+  },
+  price: {
+    //type: Sequelize.DECIMAL(10, 2), NOTE: WILL CHANGE BACK TO DECIMAL, CURRENTLY TESTING
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    defaultValue: 'Check back soon for more info!'
+  }
+});
+
+module.exports = PokeBall;
