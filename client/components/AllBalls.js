5b6abc987fd2e312539d85409ca3488eabb3a44d
israellund <israelmlund@gmail.com>
2022-01-13 18:35:26 -0500

    working api get request, created route for AllBalls component

---
+++ client/components/AllBalls.js
@@ -0,0 +1,37 @@
+import React from "react";
+import axios from "axios";
+
+// import {allBalls} from
+
+const AllBalls = () => {
+  return <div>Hello All Balls</div>;
+  //   const fetchProducts = async () => {
+  //               const response = await axios
+  //               .get("https://fakestoreapi.com/products")
+  //               .catch((err) => {}
+};
+
+export default AllBalls;
+
+// import { useEffect } from 'react';
+
+// const Translator = () => {
+//     const [languages, setLanguages] = useState([]);
+//     useEffect(() => {
+//       getLanguages().then(data => {
+//          setLanguages(data.languages)
+//       });
+
+//       console.log(languages);
+//     }, []);
+
+//   return (
+//     <header className="translator__header">
+//         {
+//           languages.length > 0 && (
+//             languages.map( lang => <h1>{lang.language}</h1>)
+//           )
+//         }
+//     </header>
+//   );
+// };
--- server/api/pokeBalls.js
+++ server/api/pokeBalls.js
@@ -1,23 +1,23 @@
-const router = require('express').Router();
+const express = require("express");
+const router = require("express").Router();
+
 const {
-  models: { PokeBall }
-} = require('../db');
+  models: { PokeBall },
+} = require("../db");
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const pokeBalls = await PokeBall.findAll({
-      attributes: ['name', 'image', 'price', 'description', 'quantity']
-    });
-    res.json(pokeBalls);
+    let allBalls = await PokeBall.findAll();
+    res.json(allBalls);
   } catch (err) {
     next(err);
   }
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
-    const pokeBall = await PokeBall.findByPk(req.params.id);
+    let pokeBall = await PokeBall.findByPk(req.params.id);
     res.json(pokeBall);
   } catch (err) {
     next(err);
