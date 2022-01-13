060fc9a3bdff6130243ff37937e986283d4595db
Sam Kanan <samkanan@gmail.com>
2022-01-13 14:30:54 -0600

    updated pokeball data file, updated pokeball model

--- server/db/models/PokeBall.js
+++ server/db/models/PokeBall.js
@@ -1,29 +1,29 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const PokeBall = db.define("pokeBall", {
+const PokeBall = db.define('pokeBall', {
   name: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false,
+    allowNull: false
   },
   image: {
     type: Sequelize.STRING,
-    defaultValue: "../../../../public/images/image-coming-soon.jpeg",
+    defaultValue: '../../../../public/images/image-coming-soon.jpeg'
   },
   price: {
     //type: Sequelize.DECIMAL(10, 2), NOTE: WILL CHANGE BACK TO DECIMAL, CURRENTLY TESTING
-    type: Sequelize.STRING,
-    allowNull: false,
+    type: Sequelize.DECIMAL,
+    allowNull: false
   },
   quantity: {
     type: Sequelize.INTEGER,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.TEXT,
-    defaultValue: "Check back soon for more info!",
-  },
+    defaultValue: 'Check back soon for more info!'
+  }
 });
 
 module.exports = PokeBall;
