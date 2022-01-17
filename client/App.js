095dca1c2c05a3b8d396387e4619bdbdc3237d93
israellund <israelmlund@gmail.com>
2022-01-17 15:51:04 -0500

    get all and single express routes for Users/Balls

--- client/App.js
+++ client/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Views
-import HomeScreen from './views/HomeScreen';
-import ProductScreen from './views/ProductScreen';
-import CartScreen from './views/CartScreen';
+import HomeScreen from "./views/HomeScreen";
+import ProductScreen from "./views/ProductScreen";
+import CartScreen from "./views/CartScreen";
 
 // Components
-import Navbar from './components/Navbar.js';
-import Backdrop from './components/Backdrop.js';
-import SideDrawer from './components/SideDrawer.js';
+import Navbar from "./components/Navbar.js";
+import Backdrop from "./components/Backdrop.js";
+import SideDrawer from "./components/SideDrawer.js";
 
 function App() {
   const [sideToggle, setSideToggle] = useState(false);
@@ -23,9 +23,9 @@ function App() {
       <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
       <main>
         <Switch>
-          <Route exact path='/' component={HomeScreen} />
-          <Route exact path='/product/:id' component={ProductScreen} />
-          <Route exact path='/cart' component={CartScreen} />
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
         </Switch>
       </main>
     </Router>
@@ -33,3 +33,34 @@ function App() {
 }
 
 export default App;
+
+// import ‘./App.css’;
+// import { useState } from ‘react’;
+// import { BrowserRouter as Router, Switch, Route } from ‘react-router-dom’;
+// // Views
+// import HomeScreen from ‘./screens/HomeScreen’;
+// import ProductScreen from ‘./screens/ProductScreen’;
+// import CartScreen from ‘./screens/CartScreen’;
+// // Components
+// import Navbar from ‘./components/Navbar.js’;
+// import Backdrop from ‘./components/Backdrop.js’;
+// import SideDrawer from ‘./components/SideDrawer.js’;
+// function App() {
+//   const [sideToggle, setSideToggle] = useState(false);
+//   return (
+//     <Router>
+//       <Navbar click={() => setSideToggle(true)} />
+//       <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+//       <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+//       <main>
+//         <Switch>
+//           <Route exact path=‘/’ component={HomeScreen} />
+//           <Route exact path=‘/product/:id’ component={ProductScreen} />
+//           <Route exact path=‘/cart’ component={CartScreen} />
+//         </Switch>
+//       </main>
+//     </Router>
+//   );
+// }
+
+//export default App;
