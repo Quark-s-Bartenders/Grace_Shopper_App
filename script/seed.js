ee1dca1ba519cde3ac4339e81ed347d453a18f9f
Sam Kanan <samkanan@gmail.com>
2022-01-18 15:49:05 -0600

    working on heroku issues

--- script/seed.js
+++ script/seed.js
@@ -1,74 +1,8 @@
-// const {
-//   db,
-//   models: { User, PokeBall, Order }
-// } = require('../server/db');
-
-// /**
-//  * seed - this function clears the database, updates tables to
-//  *      match the models, and populates the database.
-//  */
-// async function seed() {
-//   await db.sync({ force: true }); // clears db and matches models to tables
-//   console.log('db synced!');
-
-//   // Creating Users
-//   const users = await Promise.all([
-//     User.create({ username: 'cody', password: '123' }),
-//     User.create({ username: 'murphy', password: '123' })
-//   ]);
-
-//   console.log(`seeded ${users.length} users`);
-//   console.log(`seeded successfully`);
-//   return {
-//     users: {
-//       cody: users[0],
-//       murphy: users[1]
-//     }
-//   };
-// }
-
-// /*
-//  We've separated the `seed` function from the `runSeed` function.
-//  This way we can isolate the error handling and exit trapping.
-//  The `seed` function is concerned only with modifying the database.
-// */
-// async function runSeed() {
-//   console.log('seeding...');
-//   try {
-//     await seed();
-//   } catch (err) {
-//     console.error(err);
-//     process.exitCode = 1;
-//   } finally {
-//     console.log('closing db connection');
-//     await db.close();
-//     console.log('db connection closed');
-//   }
-// }
-
-// /*
-//   Execute the `seed` function, IF we ran this module directly (`node seed`).
-//   `Async` functions always return a promise, so we can use `catch` to handle
-//   any errors that might occur inside of `seed`.
-// */
-// if (module === require.main) {
-//   runSeed();
-// }
-
-// // we export the seed function for testing purposes (see `./seed.spec.js`)
-// module.exports = seed;
-
-'use strict';
-const pokeObject = require('../pokeball.js');
+const pokeBalls = require('../pokeball.js');
 const {
   db,
   models: { User, PokeBall, Order }
 } = require('../server/db');
-
-/**
- * seed - this function clears the database, updates tables to
- *      match the models, and populates the database.
- */
 
 let admins = [
   {
@@ -157,37 +91,42 @@ let admins = [
   }
 ];
 
+/**
+ * seed - this function clears the database, updates tables to
+ *      match the models, and populates the database.
+ */
+
 async function seed() {
   await db.sync({ force: true }); // clears db and matches models to tables
   console.log('db synced!');
 
+  //Creating Users
   try {
     await Promise.all(
       admins.map((admin) => {
         User.create({
           username: admin.username,
-          password: admin.password,
-          firstName: admin.firstName,
-          lastName: admin.lastName,
-          address: admin.address,
-          city: admin.city,
-          state: admin.state,
-          postalCode: admin.postalCode,
-          phone: admin.phone,
-          isAdmin: admin.isAdmin,
-          ccNum: admin.ccNum,
-          cvv: admin.cvv
+          password: admin.password
+          // firstName: admin.firstName,
+          // lastName: admin.lastName,
+          // address: admin.address,
+          // city: admin.city,
+          // state: admin.state,
+          // postalCode: admin.postalCode,
+          // phone: admin.phone,
+          // isAdmin: admin.isAdmin,
+          // ccNum: admin.ccNum,
+          // cvv: admin.cvv
         });
       })
     );
   } catch (error) {
     console.log(error);
   }
-  //Creating Users
 
   try {
     await Promise.all(
-      pokeObject.PokeBalls.map((ball) => {
+      pokeBalls.map((ball) => {
         PokeBall.create({
           name: ball.name,
           image: ball.image,
