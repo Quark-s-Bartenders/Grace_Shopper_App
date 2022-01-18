b5bc196c2252271c7ac92b5d5749c14edc96e158
Sam Kanan <samkanan@gmail.com>
2022-01-18 08:53:03 -0600

    cleaning up files

--- client/store/index.js
+++ client/store/index.js
@@ -1,28 +1,22 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Reducers
-import { cartReducer } from "./reducers/cartReducers";
-import {
-  getProductDetailsReducer,
-  getProductsReducer,
-} from "./reducers/productReducers";
-import allBallsReducer from "./reducers/allBallsReducer";
+// import { cartReducer } from './reducers/cartReducers';
+// import { getProductDetailsReducer, getProductsReducer } from './reducers/productReducers';
+import allBallsReducer from './reducers/allBallsReducer';
 
 const reducer = combineReducers({
-  cart: cartReducer,
-  getProducts: getProductsReducer,
-  getProductDetails: getProductDetailsReducer,
-  pokeballs: allBallsReducer,
+  // cart: cartReducer,
+  // getProducts: getProductsReducer,
+  // getProductDetails: getProductDetailsReducer,
+  pokeballs: allBallsReducer
 });
 
 const middleware = [thunk];
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
-export * as actionCreators from "./actions/allBalls";
+export * as actionCreators from './actions/allBalls';
--- client/views/HomeScreen.js
+++ client/views/HomeScreen.js
@@ -1,25 +1,14 @@
-import React from "react";
-import { useEffect } from "react";
-import "./viewStyles/HomeScreen.css";
+import React, { useEffect } from 'react';
+import './viewStyles/HomeScreen.css';
 
-import Product from "../components/Product.js";
+import Product from '../components/Product.js';
 
-import { getProducts as listProducts } from "../store/actions/productActions";
-
-import { useSelector, useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import { actionCreators } from "../store";
-import { setPokeballs, fetchPokeballs } from "../store/actions/allBalls";
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../store';
+import { setPokeballs, fetchPokeballs } from '../store/actions/allBalls';
 
 const HomeScreen = () => {
-  // const dispatch = useDispatch();
-  // const getProducts = useSelector((state) => state.getProducts);
-  // const { products, loading, error } = getProducts;
-
-  // useEffect(() => {
-  //   dispatch(listProducts());
-  // }, [dispatch]);
-
   const pokeBalls = useSelector((state) => state.pokeballs);
   const dispatch = useDispatch();
   const { setPokeballs } = bindActionCreators(actionCreators, dispatch);
@@ -31,9 +20,9 @@ const HomeScreen = () => {
   console.log(pokeBalls);
 
   return (
-    <div className="homescreen">
-      <h2 className="homescreen__title">Latest Poke Balls</h2>
-      <div className="homescreen__products">
+    <div className='homescreen'>
+      <h2 className='homescreen__title'>Latest Poke Balls</h2>
+      <div className='homescreen__products'>
         {pokeBalls.map((pokeball) => {
           return <Product pokeball={pokeball} />;
         })}
