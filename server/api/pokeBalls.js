abedff1e00d9e750003a726bf0809f5632e4ed50
israellund <israelmlund@gmail.com>
2022-01-17 21:15:01 -0500

    post put delete routes

--- server/api/pokeBalls.js
+++ server/api/pokeBalls.js
@@ -23,3 +23,53 @@ router.get("/:id", async (req, res, next) => {
     next(err);
   }
 });
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const ballToDelete = await PokeBall.findByPk(req.params.id);
+    if (ballToDelete) {
+      res.send(ballToDelete);
+      await ballToDelete.destroy();
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, image, price, quantity, description } = req.body;
+    let ball = await PokeBall.create({
+      name,
+      image,
+      price,
+      quantity,
+      description,
+    });
+    ball = await ball.reload();
+    res.json(ball);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { name, image, price, quantity, description } = req.body;
+    const ball = await PokeBall.findByPk(req.params.id);
+
+    const updatedBall = await ball.update({
+      name,
+      image,
+      price,
+      quantity,
+      description,
+    });
+    res.send(updatedBall);
+  } catch (error) {
+    next(error);
+  }
+});
